7358190483ebc1dddf974bc7216e744607b7fa59
jhryu1208 <user-email>
2020-03-21 01:15:28 +0900

    JH_NMBingo_Codes

---
+++ NormalBingoGame/NormalBingoGame/NMBingo.cpp
@@ -0,0 +1,151 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	srand((unsigned int)time(0));
+
+	// 5X5 빙고판
+	int iNumber[25] = {};
+
+	for (int i = 0; i < 25; ++i)
+	{
+		iNumber[i] = i + 1;
+	}
+
+	int idx1, idx2, iTemp;
+
+	// 숫자를 섞어준다.
+	for (int i = 0; i < 100; ++i)
+	{
+		idx1 = rand() % 25;
+		idx2 = rand() % 25;
+
+		iTemp = iNumber[idx1];
+		iNumber[idx1] = iNumber[idx2];
+		iNumber[idx2] = iTemp;
+	}
+
+	int iBingo = 0;
+
+	while (1)
+	{
+		// *이 생긴
+		// 빙고판을 즉각적으로 업데이트하기위해서 결과화면을 지워주고 새로만든다.
+		system("cls");
+
+		// 숫자 5X5로 출력한다.
+		for (int i = 0; i < 5; ++i)
+		{
+			for (int j = 0; j < 5; ++j)
+			{
+				// 숫자를 *로 업데이트
+				if (iNumber[i * 5 + j] == INT_MAX)
+					cout << "*\t";
+				// 그대로 출력
+				else
+					cout << iNumber[i * 5 + j] << "\t";
+			}
+			cout << endl;
+		}
+
+		cout << "Bingo Line :" << iBingo << endl;
+
+		// 줄수가 5 이상일 경우 게임을 종료한다.
+		if (iBingo >= 5)
+			break;
+
+		cout << "숫자를 입력하세요 (0: 종료)";
+
+		int iInput;
+		cin >> iInput;
+
+		if (iInput == 0)
+			break;
+
+		else if (iInput < 1 || iInput>25)
+			continue;
+
+		// 중복입력을 체크하기 위한 변수이다. 기본적으로 중복되었다라고 하기위해 true로 잡았다.
+		bool bAcc = true;
+
+		// 모든 숫자를 차례대로 검사해서 입력한 숫자와 같은 숫자가 있는지를 찾아낸다.
+		for (int i = 0; i < 25; ++i)
+		{
+			// 같은 숫자가 있을 경우
+			if (iInput == iNumber[i])
+			{
+				// 숫자를 찾았을 경우 중복된 숫자가 아니므로
+				// bAcc 변수를 false로 만들어준다.
+				bAcc = false;
+
+				// 숫자를 *로 변경하기 위해 특수한 값인 INT_MAX로 해준다.
+				// INT_MAX에는 특수값(2147483647)이 들어있다.
+				iNumber[i] = INT_MAX;
+
+				// 더이상 다른 숫자를 찾아볼 필요가 없으므로
+				// for문을 빠져나간다.
+				break;
+			}
+		}
+
+		// bAcc 변수가 true일 경우 중복된 숫자를 입력해서 숫자를 *로
+		// 바꾸지 못했으므로 다시 입력받게  continue 해준다.
+		if (bAcc)
+			continue;
+
+		// 빙고 줄 수를 체크하는 것은 매번 숫자를 입력할때마다 처음부터
+		// 새로 카운트 할것이다. 그러므로 iBingo 변수를 0으로 매번
+		// 초기화하고 새롭게 줄 수를 구해주도록 한다.
+		iBingo = 0;
+
+		// 가로,세로 줄 수를 구해준다.
+		int iStar1 = 0, iStar2 = 0;
+		for (int i = 0; i < 5; ++i)
+		{
+			// 한줄을 체크하기 전에 먼저 0으로 별 개수를 초기화한다.
+			iStar1 = iStar2 = 0;
+			for (int j = 0; j < 5; ++j)
+			{
+				// 가로 별 개수를 구해준다.
+				if (iNumber[i * 5 + j] == INT_MAX)
+					++iStar1;
+
+				// 세로 별 개수를 구해준다.
+				if (iNumber[i + 5 * j] == INT_MAX)
+					++iStar2;
+			}
+
+			// j for문을 빠져나오고 나면 현재 줄의 가로 별 개수가 몇개인지
+			// iStar1 변수에 들어가게 된다.
+			// iStar1 값이 5이면 한줄이 모두 *이라는 의미이므로 빙고 줄 카운트를 추가한다.
+			if (iStar1 == 5 || iStar2 == 5)
+				++iBingo;
+		}
+
+		// 왼쪽 상단 -> 오른쪽 하단 대각선 체크
+		iStar1 = 0;
+		for (int i = 0; i < 25; i += 6)
+		{
+			if (iNumber[i] == INT_MAX)
+				++iStar1;
+		}
+
+		if (iStar1 == 5)
+			++iBingo;
+
+		// 오른쪽 상단 -> 왼쪽 하단 대각선 체크
+		iStar1 = 0;
+		for (int i = 4; i < 25; i += 4)
+		{
+			if (iNumber[i] == INT_MAX)
+				++iStar1;
+		}
+
+		if (iStar1 == 5)
+			++iBingo;
+	}
+	return 0;
+}
